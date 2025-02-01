d959ee76c4b0879c3e72f6be1cc424b3c42480f3
fake-dp <fakecoko92@gmail.com>
2025-02-01 15:55:25 +0900

    5회차 작업내역
    에피그램 상세, 조회, 수정, 댓글 api 연동 및 기능구현

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import React from "react";
-import AuthForm from "@/components/AuthForm";
-import { login } from "@/lib/apis/auth";
-import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "next/navigation";
+import React from 'react';
+import AuthForm from '@/components/AuthForm';
+import { login } from '@/lib/apis/auth';
+import { useAuthStore } from '@/store/authStore';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
   const setAuth = useAuthStore((state) => state.login);
@@ -12,20 +12,17 @@ export default function LoginPage() {
 
   const handleLogin = async (data: { email: string; password: string }) => {
     try {
-
       const response = await login(data);
 
       const { accessToken, refreshToken, user } = response;
       setAuth(accessToken, refreshToken, user);
 
-      router.push("/");
+      router.push('/');
     } catch (error) {
-      console.error("로그인 실패:", error);
-      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+      console.error('로그인 실패:', error);
+      alert('로그인에 실패했습니다. 다시 시도해주세요.');
     }
   };
 
-  return (
-      <AuthForm type="login" onSubmit={handleLogin} />
-  );
+  return <AuthForm type="login" onSubmit={handleLogin} />;
 }
